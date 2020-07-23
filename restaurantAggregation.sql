857b7a717450d91bda216d94ae8beb410b5a069a
Micah Peterson <micahpeterson@live.com>
2020-07-23 15:38:36 -0500

    aggregation and sorting

--- restaurantAggregation.sql
+++ restaurantAggregation.sql
@@ -1,2 +1,10 @@
 
 -- aggregation exercises
+
+-- 1. list restaurants by closest distance
+SELECT name, distance FROM restaurant ORDER BY distance;
+
+-- 2. list top 2  (limit) restaurants by clostest distance
+SELECT name, distance FROM restaurant ORDER BY distance LIMIT 2;
+
+-- 3.
