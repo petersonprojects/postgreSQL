1229c31a5a1534b79f2685aef7c0f8a22da42cf0
Micah Peterson <micahpeterson@live.com>
2020-07-23 15:32:48 -0500

    SQL ex1 finished

--- restaurantSeed.sql
+++ restaurantSeed.sql
@@ -10,6 +10,7 @@
 
 -- this changes chili's category to American from Thai
 -- UPDATE restaurant SET category = 'American' WHERE id = 4;
+-- UPDATE restaurant SET favorite_dish = 'Southwestern Eggrolls' WHERE id = 4;
 
 -- 1 query for names of restaurants that I gave 5 stars
 SELECT name FROM restaurant WHERE stars = 5;
@@ -23,7 +24,19 @@ SELECT id FROM restaurant WHERE name = 'Chuys';
 -- 4 query for Indian restaurants
 SELECT name FROM restaurant WHERE category = 'Indian';
 
-SELECT name FROM restaurant WHERE category = 'Indian';
-SELECT name FROM restaurant WHERE category = 'Indian';
-SELECT name FROM restaurant WHERE category = 'Indian';
+-- 5 query for all restaurants that do takeout
+SELECT name FROM restaurant WHERE takeout = TRUE;
+
+-- 6 query for all restaurants that do takeout and indian
+SELECT name FROM restaurant WHERE takeout = TRUE AND category = 'Indian';
+
+-- 7 query for all restaurants that are within 2 miles
+SELECT name FROM restaurant WHERE distance <= 2;
+
+-- 8 query for all restaurants that you haven't eaten at in the last week
+SELECT name FROM restaurant WHERE last_visit <= '2020-07-16';
+
+-- 9 query for all restaurants that you haven't eaten at in the last week and had 5 stars
+SELECT name FROM restaurant WHERE last_visit <= '2020-07-16' AND stars = 5;
+
 
